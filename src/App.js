063c964ee72ee49cf97b71326a625f7c09a1c588
Kristian Vassilev <kvasilev@devrix.com>
2021-11-15 14:31:48 +0200

    Added router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import WelcomeWorld from "./components/WelcomeWorld";
 import GameCatalog from "./components/GameCatalog";
 import CreateGame from "./components/CreateGame";
 
 function App() {
+	const [page, setPage] = useState('/home');
+
 	const routes = {
-		'/home': WelcomeWorld,
-		'/games': GameCatalog,
-		'/create-game': CreateGame,
-	}
+		"/home": <WelcomeWorld />,
+		"/games": <GameCatalog />,
+		"/create-game": <CreateGame />,
+	};
+
+	const navigationChangeHandler = (path) => {
+		console.log(path);
+		setPage(path);
+	};
 
 	return (
 		<div id="box">
-			<Header />
+			<Header navigationChangeHandler={navigationChangeHandler} />
 
 			<main id="main-content">
-				<WelcomeWorld />
+				{ routes[page] || <h2>No Page Found! </h2>}
 			</main>
 		</div>
 	);
