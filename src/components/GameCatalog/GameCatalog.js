77909da97c6e4f2ca5898535cb633a0f3b3c086b
Kristian Vassilev <kvasilev@devrix.com>
2021-11-15 15:42:22 +0200

    Show no games yet

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -18,11 +18,11 @@ const GameCatalog = () => {
 		<section id="catalog-page">
 			<h1>All Games</h1>
 
-			{games.map((x) => (
-				<GameCard game={x} />
-			))}
+			{ games.length > 0
+				? games.map(x => ( <GameCard key={x._id} game={x} />))
+				: <h3 className="no-articles">No games yet</h3>
+			 }
 
-			<h3 className="no-articles">No games found</h3>
 		</section>
 	);
 };
